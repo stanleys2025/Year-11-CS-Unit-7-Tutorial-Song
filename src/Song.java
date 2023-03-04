44a3275115d790479c9e20e6b3223a6780d36612
sustanley <stanleys2025@student.cis.edu.hk>
2023-03-04 22:41:43 +0800

    hhhh

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,45 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> listeners = new ArrayList();
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+
+
+    public int howMany(ArrayList<String> users){
+        int newListeners = 0;
+        users.replaceAll(String::toLowerCase);
+        for(String user : users){
+            if(!listeners.contains(user)){
+                newListeners++;
+                listeners.add(user);
+            }
+        }
+        return newListeners;
+    }
+
+
+
 }
